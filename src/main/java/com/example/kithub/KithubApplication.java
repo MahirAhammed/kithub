a1320659fa7ea7794eee9cc06273ef039e4ca7a5
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-03 21:55:30 +0200

    Initial commit with folder structured and default project files

---
+++ src/main/java/com/example/kithub/KithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.kithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kithub/KithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

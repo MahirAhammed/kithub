0d905ac7a4f144ccc719549a92ceed958dd3dfb9
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-08 23:20:02 +0200

    feat: Implemented CategoryController with basic GET, POST, PUT, DELETE endpoints

---
+++ src/main/java/com/example/kithub/category/CategoryController.java
@@ -0,0 +1,38 @@
+package com.example.kithub.category;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    @GetMapping()
+    public ResponseEntity<List<Category>> getAllCategories(){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable long id){
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity addCategory(@RequestBody String category){
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateCategory(@RequestBody String category){
+        return ResponseEntity.ok().build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCategory(){
+        return ResponseEntity.ok().build();
+
+    }
+}

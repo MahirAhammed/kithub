3164be7c0929ae325f55c73e0f17841d8df955ed
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-09 18:09:35 +0200

    fix: Modified responses of every request endpoints by calling appropriate handler in CategoryController

--- src/main/java/com/example/kithub/category/CategoryController.java
+++ src/main/java/com/example/kithub/category/CategoryController.java
@@ -1,5 +1,11 @@
 package com.example.kithub.category;
 
+import com.example.kithub.category.commandhandlers.AddCategoryHandler;
+import com.example.kithub.category.commandhandlers.DeleteCategoryHandler;
+import com.example.kithub.category.commandhandlers.UpdateCategoryHandler;
+import com.example.kithub.category.queryhandlers.GetAllCategoriesHandler;
+import com.example.kithub.category.queryhandlers.GetCategoryByIdHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,30 +15,47 @@ import java.util.List;
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
 
+    private final GetAllCategoriesHandler getAllCategoriesHandler;
+    private final GetCategoryByIdHandler getCategoryByIdHandler;
+    private final AddCategoryHandler addCategoryHandler;
+    private final UpdateCategoryHandler updateCategoryHandler;
+    private final DeleteCategoryHandler deleteCategoryHandler;
+
+    @Autowired
+    public CategoryController(GetAllCategoriesHandler getAllCategoriesHandler, GetCategoryByIdHandler getCategoryByIdHandler, AddCategoryHandler addCategoryHandler, UpdateCategoryHandler updateCategoryHandler, DeleteCategoryHandler deleteCategoryHandler) {
+        this.getAllCategoriesHandler = getAllCategoriesHandler;
+        this.getCategoryByIdHandler = getCategoryByIdHandler;
+        this.addCategoryHandler = addCategoryHandler;
+        this.updateCategoryHandler = updateCategoryHandler;
+        this.deleteCategoryHandler = deleteCategoryHandler;
+    }
+
     @GetMapping()
     public ResponseEntity<List<Category>> getAllCategories(){
-        return ResponseEntity.ok().build();
+        return getAllCategoriesHandler.execute(null);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Category> getCategoryById(@PathVariable long id){
-        return ResponseEntity.ok().build();
+        return getCategoryByIdHandler.execute(id);
     }
 
     @PostMapping()
-    public ResponseEntity addCategory(@RequestBody Category category){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Category> addCategory(@RequestBody CategoryDTO category){
+        return addCategoryHandler.execute(category) ;
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity updateCategory(@RequestBody Category category){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Category> updateCategory(@PathVariable long id, @RequestBody Category category){
+
+        category.setCategoryId(id);
+        return updateCategoryHandler.execute(category);
 
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteCategory(){
-        return ResponseEntity.ok().build();
+    public ResponseEntity deleteCategory(@PathVariable long id){
+        return deleteCategoryHandler.execute(id);
 
     }
 }

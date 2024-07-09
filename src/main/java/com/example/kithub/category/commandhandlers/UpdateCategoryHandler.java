bd531a1d73957f54481295fbd5be151dff58070e
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-09 17:32:56 +0200

    Implemented command handler for updating categories

--- src/main/java/com/example/kithub/category/commandhandlers/UpdateCategoryHandler.java
+++ src/main/java/com/example/kithub/category/commandhandlers/UpdateCategoryHandler.java
@@ -1,4 +1,38 @@
 package com.example.kithub.category.commandhandlers;
 
-public class UpdateCategoryHandler {
+import com.example.kithub.Command;
+import com.example.kithub.category.Category;
+import com.example.kithub.category.CategoryDTO;
+import com.example.kithub.category.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UpdateCategoryHandler implements Command<Category, Category> {
+
+    private CategoryRepository repository;
+
+    @Autowired
+    public UpdateCategoryHandler(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ResponseEntity<Category> execute(Category category) {
+
+        Optional<Category> newCategory = repository.findById(category.getCategoryId());
+
+        if (newCategory.isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+        }
+
+        repository.save(category);
+
+        return ResponseEntity.status(HttpStatus.OK).body(category);
+    }
 }

1b0e80bd8be9b35766a377060ab8423af588baa1
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-10 11:53:28 +0200

    feat: Created Product entity with attributes and relation with Category entity

---
+++ src/main/java/com/example/kithub/product/Product.java
@@ -0,0 +1,57 @@
+package com.example.kithub.product;
+
+import com.example.kithub.category.Category;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "product_id")
+    private UUID productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "amount")
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "category", referencedColumnName = "id")
+    private Category category;
+
+    @Column(name = "supplier")
+    private String supplier;
+
+    @CreationTimestamp
+    private LocalDateTime timeCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime timeUpdated;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection
+    private List<Region> region;
+}

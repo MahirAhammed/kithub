363be87a99ddbf86d551be3dffb6bacf9b1bafbb
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-15 14:58:36 +0200

    feat: Implemented command handler for deleting products

---
+++ src/main/java/com/example/kithub/product/commandhandlers/DeleteProductHandler.java
@@ -0,0 +1,36 @@
+package com.example.kithub.product.commandhandlers;
+
+import com.example.kithub.Command;
+import com.example.kithub.product.Product;
+import com.example.kithub.product.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class DeleteProductHandler implements Command<String, Void>{
+
+    private final ProductRepository repository;
+
+    @Autowired
+    public DeleteProductHandler(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ResponseEntity<Void> execute(String id) {
+        Optional<Product> product = repository.findById(UUID.fromString(id));
+        if (product.isEmpty()){
+            throw new RuntimeException("Product does not exist");
+        }
+
+        repository.delete(product.get());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
+

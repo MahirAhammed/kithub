90591ad04f7becdb4942a7a6945c4e40112118be
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-15 11:28:37 +0200

    feat: Implemented query handler to retrieve product by id

---
+++ src/main/java/com/example/kithub/product/queryhandlers/GetProductByIdHandler.java
@@ -0,0 +1,35 @@
+package com.example.kithub.product.queryhandlers;
+
+import com.example.kithub.Query;
+import com.example.kithub.product.Product;
+import com.example.kithub.product.ProductDTO;
+import com.example.kithub.product.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class GetProductByIdHandler implements Query<String, ProductDTO> {
+
+    private ProductRepository repository;
+
+    @Autowired
+    public GetProductByIdHandler(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ResponseEntity<ProductDTO> execute(String id) {
+        Optional<Product> product = repository.findById(UUID.fromString(id));
+
+        if (product.isEmpty()){
+            throw new RuntimeException("Product does not exist");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ProductDTO(product.get()));
+    }
+}

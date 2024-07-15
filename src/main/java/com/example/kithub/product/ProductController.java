489457dee2360e4b3799a9090e82435bc269199d
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-15 15:00:46 +0200

    fix: Modified ProductController with appropriate handlers set for each endpoint

--- src/main/java/com/example/kithub/product/ProductController.java
+++ src/main/java/com/example/kithub/product/ProductController.java
@@ -1,50 +1,67 @@
 package com.example.kithub.product;
 
-import org.springframework.data.repository.query.Param;
+import com.example.kithub.product.commandhandlers.AddProductHandler;
+import com.example.kithub.product.commandhandlers.DeleteProductHandler;
+import com.example.kithub.product.queryhandlers.GetProductByIdHandler;
+import com.example.kithub.product.queryhandlers.GetProductsHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
+    private final AddProductHandler addProductHandler;
+    private final GetProductsHandler getProductsHandler;
+    private final GetProductByIdHandler getProductByIdHandler;
+    private final DeleteProductHandler deleteProductHandler;
+
+
+    @Autowired
+    public ProductController(AddProductHandler addProductHandler, GetProductsHandler getProductsHandler, GetProductByIdHandler getProductByIdHandler, DeleteProductHandler deleteProductHandler) {
+        this.addProductHandler = addProductHandler;
+        this.getProductsHandler = getProductsHandler;
+        this.getProductByIdHandler = getProductByIdHandler;
+        this.deleteProductHandler = deleteProductHandler;
+    }
+
     @GetMapping
-    public ResponseEntity<ProductDTO> getProductsByCriteria(
-            @RequestHeader(value = "Region", defaultValue = "ZA") String region,
+    public ResponseEntity<List<ProductDTO>> getProductsByCriteria(
+            @RequestHeader(value = "Region", defaultValue = "ZA",required = false) String region,
             @RequestParam(required = false) String nameOrDescription,
             @RequestParam(required = false) String category,
             @RequestParam(required = false) String orderBy,
             @RequestParam(required = false) String supplier
     ){
-        return ResponseEntity.ok().build();
+        return getProductsHandler.execute(new ProductRequest(region, nameOrDescription, category, orderBy, supplier));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> getProductById(@PathVariable String id){
-        return ResponseEntity.ok().build();
+        return getProductByIdHandler.execute(id);
 
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> addProduct(
-            @RequestBody ProductDTO product,
-            @RequestHeader(value = "Region", defaultValue = "ZA") String region){
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product){
+        return addProductHandler.execute(product);
 
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProductDTO> updateProduct(@PathVariable String id, @RequestBody ProductDTO product){
+    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product product){
+        product.setProductId(UUID.fromString(id));
         return ResponseEntity.ok().build();
 
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable String id){
-        return ResponseEntity.ok().build();
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        return deleteProductHandler.execute(id);
 
     }
 

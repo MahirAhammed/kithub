928c2433f47a38d93d9fe251be8133261e47cf54
Mahir Ahammed <itsmahir24@gmail.com>
2024-07-16 16:28:24 +0200

    feat: Implemented category validator, included appropriate exception handlers

--- src/main/java/com/example/kithub/product/ProductValidator.java
+++ src/main/java/com/example/kithub/product/ProductValidator.java
@@ -1,5 +1,9 @@
 package com.example.kithub.product;
 
+import com.example.kithub.category.Category;
+import com.example.kithub.exceptions.ErrorMessage;
+import com.example.kithub.exceptions.InvalidFieldException;
+
 import java.util.EnumSet;
 import java.util.List;
 
@@ -10,39 +14,44 @@ public class ProductValidator {
         validateName(product.getProductName());
         validatePrice(product.getPrice());
         validateQuantity(product.getQuantity());
-        // TODO: invalid category
-
-        //TODO: proper exception handling
-
         validateRegions(product.getRegions());
+    }
+
+    public static void categoryValidator(Category category, List<Category> categories){
+        long id = category.getCategoryId();
+
+        for (Category c : categories){
+            if (id == c.getCategoryId()){
+                return;
+            }
+        }
+
+        throw new InvalidFieldException("Provided category id does not exist");
     }
 
     private static void validateName(String name){
         if (name.isEmpty() || name.isBlank()){
-            throw new RuntimeException("Product name cannot be empty");
+            throw new InvalidFieldException(ErrorMessage.PRODUCT_NAME_EMPTY.getMessage());
         }
     }
 
     private static void validatePrice(double price){
         if (price <= 0){
-            throw new RuntimeException("Price amount is invalid");
+            throw new InvalidFieldException(ErrorMessage.PRODUCT_QUANTITY_INVALID.getMessage());
         }
     }
 
     private static void validateQuantity(int quantity){
         if (quantity <= 0){
-            throw new RuntimeException("Product quantity is invalid");
+            throw new InvalidFieldException(ErrorMessage.PRODUCT_QUANTITY_INVALID.getMessage());
+
         }
-    }
-
-    private static void validateCategory(){
-
     }
 
     private static void validateRegions(List<Region> regions){
         for (Region region: regions){
             if (!EnumSet.allOf(Region.class).contains(region)){
-                throw new RuntimeException("An invalid region was provided");
+                throw new InvalidFieldException(String.format("%s is not an available region", region.toString()));
             }
         }
 

a53768c38f7888783e3c01419dc3450b139947c5
Mahir Ahammed <itsmahir24@gmail.com>
2024-12-14 20:54:46 +0200

    feat: Included custom exception handling for invalid user inputs

--- src/main/java/com/example/kithub/exceptions/ErrorMessage.java
+++ src/main/java/com/example/kithub/exceptions/ErrorMessage.java
@@ -11,7 +11,9 @@ public enum ErrorMessage {
     PRODUCT_NAME_EMPTY("Product name cannot be empty"),
     PRODUCT_QUANTITY_INVALID("Numerical values cannot be negative or zero"),
     CATEGORY_NOT_FOUND("No such cateogory exists"),
-    USERNAME_EXISTS("User with this username already exists");
+    USERNAME_EXISTS("User with this username already exists"),
+    BLANK_USER_INPUTS("Username or password cannot be blank or empty"),
+    INVALID_PASSWORD("Password should be at least 6 characters long");
 
     private final String message;
 
